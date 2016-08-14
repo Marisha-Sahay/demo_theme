b6bbb254c639f33f283def7f6630ab2eb2119566
Marisha Sahay <marishasahay@gmail.com>
2016-08-14 11:16:38 -0700

    add theme for the website

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
+
 def welcome
 end
+def sign_up
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'pages#welcome'
+  get '/sign_up', to:'pages#sign_up'
+  get '/portfolios', to: 'portfolios#index'
 end
